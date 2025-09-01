972ddd5331b8be6b15c0ca99a489bac4b4ad2040
robinlovelace <rob00x@gmail.com>
2025-09-01 16:15:27 +0100

    Add and render local files

---
+++ R/utils.R
@@ -0,0 +1,8 @@
+#' Course Utilities
+#'
+#' A collection of utility functions for the course.
+#'
+#' @export
+hello_course <- function() {
+  message("Welcome to Data Science and Digital Tools for Transport Planning!")
+}
